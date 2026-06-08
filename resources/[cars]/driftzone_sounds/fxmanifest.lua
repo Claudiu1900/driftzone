4ea24766bc6106a05a134b529e2af55517e609ef
Claudiu1900 <refreshexx@gmail.com>
2026-06-08 23:39:05 +0300

    Added Vehicles Custom Sounds

---
+++ resources/[cars]/driftzone_sounds/fxmanifest.lua
@@ -0,0 +1,61 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DriftZone'
+description 'DriftZone Custom Vehicle Sounds'
+version '1.0.0'
+
+files {
+    'sfx/dlc_brabus850/brabus850.awc',
+    'sfx/dlc_brabus850/brabus850_npc.awc',
+
+    'sfx/dlc_gt3flat6/gt3flat6.awc',
+    'sfx/dlc_gt3flat6/gt3flat6_npc.awc',
+
+    'sfx/dlc_k20a/k20a.awc',
+    'sfx/dlc_k20a/k20a_npc.awc',
+
+    'sfx/dlc_lambov10/lambov10.awc',
+    'sfx/dlc_lambov10/lambov10_npc.awc',
+
+    'sfx/dlc_m158huayra/m158huayra.awc',
+    'sfx/dlc_m158huayra/m158huayra_npc.awc',
+
+    'sfx/dlc_m297zonda/m297zonda.awc',
+    'sfx/dlc_m297zonda/m297zonda_npc.awc',
+
+    'sfx/dlc_musv8/musv8.awc',
+    'sfx/dlc_musv8/musv8_npc.awc',
+
+    'sfx/dlc_p60b40/p60b40.awc',
+    'sfx/dlc_p60b40/p60b40_npc.awc',
+
+    'sfx/dlc_predatorv8/predatorv8.awc',
+    'sfx/dlc_predatorv8/predatorv8_npc.awc',
+
+    'sfx/dlc_rb26dett/rb26dett.awc',
+    'sfx/dlc_rb26dett/rb26dett_npc.awc',
+
+    'sfx/dlc_rotary7/rotary7.awc',
+    'sfx/dlc_rotary7/rotary7_npc.awc',
+
+    'sfx/dlc_shonen/shonen.awc',
+    'sfx/dlc_shonen/shonen_npc.awc',
+
+    'sfx/dlc_toysupmk4/toysupmk4.awc',
+    'sfx/dlc_toysupmk4/toysupmk4_npc.awc'
+}
+
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_brabus850'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gt3flat6'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_k20a'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lambov10'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_m158huayra'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_m297zonda'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_musv8'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_p60b40'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_predatorv8'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rb26dett'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rotary7'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_shonen'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_toysupmk4'
