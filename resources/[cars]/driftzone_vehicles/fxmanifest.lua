98f006f15ec144574b03178a738281c937d01ae7
Claudiu1900 <refreshexx@gmail.com>
2026-06-09 16:45:43 +0300

    Fix

--- resources/[cars]/driftzone_vehicles/fxmanifest.lua
+++ resources/[cars]/driftzone_vehicles/fxmanifest.lua
@@ -6,17 +6,11 @@ description 'DriftZone Addon Vehicles Pack'
 version '1.0.0'
 
 files {
-    '**/vehicles.meta',
-    '**/handling.meta',
-    '**/carvariations.meta',
-    '**/carcols.meta',
-    '**/dlctext.meta',
-    '**/vehiclelayouts.meta'
+    'data/**/*.meta'
 }
 
-data_file 'VEHICLE_METADATA_FILE' '**/vehicles.meta'
-data_file 'HANDLING_FILE' '**/handling.meta'
-data_file 'VEHICLE_VARIATION_FILE' '**/carvariations.meta'
-data_file 'CARCOLS_FILE' '**/carcols.meta'
-data_file 'DLC_TEXT_FILE' '**/dlctext.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' '**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'DLC_TEXT_FILE' 'data/**/dlctext.meta'
