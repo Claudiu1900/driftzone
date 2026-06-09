57ec9463ea39ce9bfc2c6ac0c0e4154a7f0a27ea
Claudiu1900 <refreshexx@gmail.com>
2026-06-09 12:09:45 +0300

    Added Vehicle Engine Toggle

--- resources/[files]/driftzone_interactions/config.lua
+++ resources/[files]/driftzone_interactions/config.lua
@@ -41,6 +41,18 @@ Config.DefaultInteractions = {
 },
 
 {
+    id = 'driftzone_licenses',
+    coords = vector3(3827.0, 3821.0, 30.0),
+    range = 2.6,
+    key = 'E',
+    text = 'Apasa E pentru inmatriculari',
+    subText = 'DriftZone Licenses',
+    marker = true,
+    event = 'driftzone_licenses:client:openFromInteraction',
+    blip = { sprite = 498, color = 3, scale = 0.80, name = 'DriftZone Licenses' }
+},
+
+{
     id = 'driftzone_races_main',
     coords = vector3(-1336.180176, -3044.254882, 13.890136),
     range = 2.8,
