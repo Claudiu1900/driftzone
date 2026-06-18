bb56163ae81e0ea869309297a620c86f6f4df2e6
Claudiu1900 <refreshexx@gmail.com>
2026-06-18 12:38:59 +0300

    Added Vinewood Sign

--- resources/[mapping]/dz_logo/fxmanifest.lua
+++ resources/[mapping]/dz_logo/fxmanifest.lua
@@ -2,12 +2,10 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'DriftZone'
-description 'Solid 3D Logo Prop'
+description 'Vinewood clear + DriftZone logo'
 version '1.0.0'
 
 this_is_a_map 'yes'
-
-client_script 'client.lua'
 
 files {
     'stream/dz_logo.ytyp'
